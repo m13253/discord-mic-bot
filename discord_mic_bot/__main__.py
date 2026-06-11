ed9ff8aa5f38b54fdea96628e0ab5bbb2047f359
Star Brilliant <coder@poorlab.com>
2026-06-11 18:44:34 +0000

    Rename dmb -> discord_mic_bot

--- discord_mic_bot/__main__.py
+++ discord_mic_bot/__main__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import dmb
+import discord_mic_bot
 
 if __name__ == "__main__":
-    dmb.main()
+    discord_mic_bot.main()
